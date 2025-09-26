3ad764a562c32ce0185778b19bbc8d1f9ff22403
Adriel Rodriguez <rodriguezadri500@gmail.com>
2025-09-26 10:12:38 -0300

    Fin del proyecto

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,12 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        secondary: "#f5741c",
-        darkBg: "#131424",
+        secondary: "#c4934bff",
+        darkBg: "#1b1c22",
       },
       backgroundImage: {
         "gradient-cover":
-          "linear-gradient(90.21deg, rgba(170, 54, 124, 0.5) -5.91%, rgba(74, 47, 189, 0.5) 111.58%)",
+          "linear-gradient(90.21deg, rgba(46, 27, 39, 0.5) -5.91%, rgba(74, 47, 189, 0.5) 111.58%)",
       },
     },
   },
